a1d03d2119bdb8a9954ee8bb9c8ea45f27f0f03a
NickAllbritton <nallbritton@sycamores.indstate.edu>
2025-03-24 03:42:14 -0400

    Tutorial 9: Drawing Lines. Changed cell to be square and board centered

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+mod view;
+
+
+fn main() -> Result<(), String> {
+  
+    let scrn_width: u32 = 1600;
+    let scrn_height: u32= 1200;
+
+    let sdl_context = sdl2::init()?;
+    let vid_subsystem = sdl_context.video()?;
+    let wnd = vid_subsystem.window("Rust!", scrn_width, scrn_height)
+        .build()
+        .unwrap();
+    
+    let mut canvas = wnd.into_canvas()
+        .build()
+        .unwrap();
+
+    let board_view: view::board_view::Renderer = view::board_view::Renderer {
+        scrn_area: sdl2::rect::Rect::new(0, 0, scrn_width, scrn_height),
+        clear_color: sdl2::pixels::Color::RGB(148, 103, 128)
+    };
+
+    let mut running: bool = true;
+    let mut event_queue = sdl_context.event_pump().unwrap();
+    
+    while running {
+
+        for event in event_queue.poll_iter() {
+            match event {
+                sdl2::event::Event::Quit {..} => {
+                    running = false;
+                }
+                _ => {}
+            }
+        }
+        
+        board_view.render(&mut canvas);
+
+        canvas.present();
+    }
+
+    Ok(())
+}
---
+++ src/view/board_view.rs
@@ -0,0 +1,44 @@
+use sdl2::rect::Rect;
+use sdl2::rect::Point;
+use sdl2::pixels::Color;
+use sdl2::render::Canvas;
+use sdl2::video::Window;
+
+
+pub struct Renderer {
+    pub scrn_area: Rect,
+    pub clear_color: Color,
+}
+
+impl Renderer {
+
+    pub fn render(&self, canvas: &mut Canvas<Window>) {
+        canvas.set_draw_color(self.clear_color);
+        canvas.fill_rect(self.scrn_area).ok().unwrap_or_default();
+
+        canvas.set_draw_color(Color::RGB(0, 0, 0));
+
+        let cell_width: i32 = self.scrn_area.h / 5;
+        let cell_height: i32 = self.scrn_area.h / 5;
+        // Board width: (4 spaces between 5 lines)x(Cell width)
+        // Centering of board involves halving the board width which is a factor of 4.
+        // So simplify to 2xCell width (and similarly for height)
+        let left: i32 = self.scrn_area.w/2 - 2*cell_width; // x-position of left side of centered board
+        let top: i32 = self.scrn_area.h/2 - 2*cell_height; // y-position of top side of centered board
+        // Then subtracting left from screen width is algebraically equivalent to the right side. 
+
+        for i in 0..5 {
+            // Draw the horizontal lines of the board 
+            canvas.draw_line(
+                Point::new(left, top + i*cell_height),
+                Point::new(self.scrn_area.w - left, top + i*cell_height)
+            ).ok().unwrap_or_default();
+            // Draw the horizontal lines of the board
+            canvas.draw_line(
+                Point::new(left + i*cell_width, top),
+                Point::new(left + i*cell_width, self.scrn_area.h - top)
+            ).ok().unwrap_or_default();
+
+        }
+    }
+}
