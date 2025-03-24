4fb348e0a019345610a1320bf6e656f0a61d9558
NickAllbritton <nallbritton@sycamores.indstate.edu>
2025-03-24 05:09:41 -0400

    added a game state struct with minimal details so far implemented. contains 25 BoardPiece's

--- src/model/game.rs
+++ src/model/game.rs
@@ -1,6 +1,24 @@
+#[derive(Clone, Copy)]
 pub enum BoardPiece {
     None,
     Red,
     Black
 }
 
+pub fn make_blank_board() -> [[BoardPiece; 5]; 5]; {
+    return [[BoardPiece::None; 5]; 5];
+}
+
+pub struct GameState {
+    pub board: [[BoardPiece; 5]; 5]
+}
+
+impl GameState {
+
+    pub fn jumbl_board(&mut self) {
+        // TODO: Actually jumble the board lol
+        self.board[1][0] = BoardPiece::Red;
+        self.board[2][0] = BoardPiece::Black;
+    }
+
+}
